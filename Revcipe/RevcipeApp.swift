219fcfa984ba2e6bc6bc0d748e8d5afe5269f003
Samuel(Xiangxin) Ji <ji.xian@northeastern.edu>
2023-10-08 15:40:56 -0400

    Xcodeproj initialized

---
+++ Revcipe/RevcipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevcipeApp.swift
+//  Revcipe
+//
+//  Created by SamuelJ on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct RevcipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
