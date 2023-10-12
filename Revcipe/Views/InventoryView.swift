4bd4a0a0999c725bca6a13f0228812757768f590
Anushka Mantri <85311996+anushkamantri@users.noreply.github.com>
2023-10-12 18:42:47 -0400

    Header for inventory of items

--- Revcipe/Views/InventoryView.swift
+++ Revcipe/Views/InventoryView.swift
@@ -10,10 +10,19 @@ import SwiftUI
 let listOfIngredients = ["Rice", "Potato", "Onion", "Pesto"]
 struct InventoryView: View {
     var body: some View {
+        ZStack {Text("**My Inventory**")
+                .padding()
+                .foregroundColor(.white)
+                .font(.system(size: 40))
+            
+        }
+        .frame(width: UIScreen.main.bounds.width * 1.5, height: 100)
+        .background((Color(#colorLiteral(red: 0.37, green: 0.65, blue: 0.98, alpha: 1))))
+        
         List {
             ForEach(listOfIngredients, id: \.self) {
                 ingredientRow in Text(ingredientRow)
-            }
+            }.foregroundColor((Color(#colorLiteral(red: 0.37, green: 0.65, blue: 0.98, alpha: 1))))
         }
     }
 }
