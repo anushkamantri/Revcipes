123f9a5af529d731aca09536452e2a9e79d3367a
Fanny Zheng <91427854+fanzzz368@users.noreply.github.com>
2023-10-15 14:21:24 -0400

    add initial view for tasteeval and tasteevalviewvm

---
+++ Revcipe/ViewModels/TasteEvalViewVM.swift
@@ -0,0 +1,18 @@
+//
+//  TasteEvalViewVM.swift
+//  Revcipe
+//
+//  Created by Fanny Zheng on 10/14/23.
+//
+
+import SwiftUI
+
+struct TasteEvalViewVM: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TasteEvalViewVM()
+}
