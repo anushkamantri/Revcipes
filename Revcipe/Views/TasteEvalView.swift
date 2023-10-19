ef4794510ad87a0e0b95e634d3223b43a1f69957
Fanny Zheng <91427854+fanzzz368@users.noreply.github.com>
2023-10-19 19:41:41 -0400

    tastevalview

--- Revcipe/Views/TasteEvalView.swift
+++ Revcipe/Views/TasteEvalView.swift
@@ -7,9 +7,55 @@
 
 import SwiftUI
 
+let listOfCuisines = ["Indian", "Mexican", "Italian", "American", "Japanese", "Chinese", "Mediterranean", "Thai", "Spanish", "European"]
+
 struct TasteEvalView: View {
+    @State private var buttonSubmit = "Submit";
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 125) {
+            Text("Select up to 5 cuisines you like")
+            
+            HStack(spacing: 50) {
+                VStack(spacing: 50) {
+                    ForEach(0..<5) { index in
+                        CuisineButton(cuisineName: listOfCuisines[index])
+                    }
+                }
+                
+                VStack(spacing: 50) {
+                    ForEach(5..<10) { index in
+                        CuisineButton(cuisineName: listOfCuisines[index])
+                    }
+                }
+                
+            }
+            
+            Button(buttonSubmit) {
+                //
+            }.padding(10)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+        }
+    }
+}
+
+
+
+struct CuisineButton: View {
+    var cuisineName: String
+    @State private var isTapped = false
+    
+    var body: some View {
+        Button(action: {
+            isTapped.toggle()
+        }) {
+            Text(cuisineName)
+                .padding(10)
+                .background(isTapped ? Color.orange : Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+        }
     }
 }
 
